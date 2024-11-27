36085b8cd39e26d4e0de8b85354c0947f8efc16a
dsmintic <dsmintic@gmail.com>
2024-11-27 15:02:19 +0100

    podešavanje eslinta i prettiera

--- my-app/eslint.config.js
+++ my-app/eslint.config.js
@@ -3,11 +3,13 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import esLintConfigPrettier from "eslint-config-prettier"
+import esLintPluginPrettier from "eslint-plugin-prettier/recommended"
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [js.configs.recommended, ...tseslint.configs.recommended, esLintConfigPrettier, esLintPluginPrettier],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
@@ -19,9 +21,12 @@ export default tseslint.config(
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
+      "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+      "prettier/prettier": [
+        "warn",
+        {
+          endOfLine: "auto",
+        },
       ],
     },
   },
