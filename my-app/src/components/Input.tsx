d3a9c26a548a233b4d366db1066372fd45e0f0b9
dsmintic <dsmintic@gmail.com>
2024-11-29 14:45:11 +0100

    Error handling 2

--- my-app/src/components/Input.tsx
+++ my-app/src/components/Input.tsx
@@ -3,13 +3,15 @@ import "./Input.css";
 interface InputProps {
   name: string;
   label: string;
+  error?: string;
 }
 
-export default function Input({ name, label }: InputProps) {
+export default function Input({ error, name, label }: InputProps) {
   return (
     <div className="Field">
       <label htmlFor={name}>{label}</label>
       <input id={name} name={name} />
+      {error && <span style={{ color: "red" }}>{error}</span>}
     </div>
   );
 }
--- my-app/src/feature/course apply/Apply.tsx
+++ my-app/src/feature/course apply/Apply.tsx
@@ -3,33 +3,55 @@ import Button from "../../components/Button";
 import Input from "../../components/Input";
 import TextArea from "../../components/TextArea";
 import Title from "../../components/Title";
-import { applyAttendants } from "../../api/attendants";
 import { useNavigate, useParams } from "react-router";
+import { useState } from "react";
+import { AttendantsRequest } from "../../model/attendants";
 
 export default function Apply() {
   const params = useParams();
   const navigate = useNavigate();
+  const [errors, setErrors] = useState<Record<keyof AttendantsRequest, string>>();
+
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-
     const formData = new FormData(event.currentTarget);
 
     const data: Record<string, unknown> = {};
+
     formData.forEach((value, name) => {
       data[name] = value;
     });
+
+    const formErrors = {};
+
+    if (!data.firstName) {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      formErrors.firstName = "Fali first name";
+    }
+
+    console.log(formErrors)
+
+    if (Object.keys(formErrors).length > 0) {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      setErrors(formErrors);
+      return;
+    }
+
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
-    applyAttendants({
+    applyAttendant({
       ...data,
       courseId: params.id!,
     }).then(() => navigate("/courses"));
   };
+
   return (
     <div>
       <Title title="Apply to course"></Title>
       <form onSubmit={onSubmit}>
-        <Input name="firstName" label="Ime" />
+        <Input name="firstName" label="Ime" error={errors?.firstName} />
         <Input name="lastName" label="Prezime" />
         <Input name="email" label="E-mail" />
         <Input name="company" label="Kompanija" />
